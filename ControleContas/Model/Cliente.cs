2add46df92b3ee0e0d0a88460b52381857a73c8d
Souzaxyz <souzaan.gabriel@gmail.com>
2023-03-22 20:43:23 -0300

    validação de dados de cliente

--- ControleContas/Model/Cliente.cs
+++ ControleContas/Model/Cliente.cs
@@ -11,5 +11,19 @@ namespace ControleContas.Model
         public string Nome { get; private set; }
         public string Cpf { get; private set; }
         public int AnoNascimento { get; private set; }
+
+        public Cliente (string nome, string cpf, int anoNascimento)
+        {
+
+            if (anoNascimento > 2005)
+                throw new Exception("Precisa ter mais de 18 anos!");
+            if (cpf.Length != 11)
+                throw new Exception("Cpf inválido, o Cpf deve possuir somente números(11 números)");
+            
+            Nome = nome;
+            Cpf = cpf;
+            AnoNascimento = anoNascimento;
+
+        }
     }
 }
--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -8,3 +8,5 @@ Conta conta3 = new Conta("654321", 2341.42m);
 
 Console.WriteLine($"O saldo total é {conta2.SaldoTotal}");
 Console.WriteLine($"A conta de maior saldo é {conta2._ContaMaiorSaldo}");
+
+Cliente cliente1 = new Cliente("Souza", "12345678910", 2000);
